01c16d47adf3c9b70252297561fe1d98fb464630
GJAA98 <79035604+GJAA98@users.noreply.github.com>
2021-03-01 17:02:34 -0400

    Add files via upload
    Crear una clase que permita ingresar valores enteros por teclado y nos muestre la tabla de multiplicar de dicho valor. Finalizar el programa al ingresar el -1.

---
+++ Practica_tabla_multiplicar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Practica_tabla_multiplicar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continuar = true;
+            while (continuar)
+            {
+                Console.WriteLine("Inserte un numero:");
+                string valor =  Console.ReadLine();
+                int numero = int.Parse(valor);
+                if(numero == -1)
+                {
+                    continuar = false;
+                }
+                else
+                {
+                    Console.WriteLine("Tabla de multiplicar de " + numero);
+                    for (int i = 1; i <=12; i++)
+                    {
+                        int resultado = numero * i;
+                        Console.WriteLine(numero + "x" + i + "=" + resultado);
+                    }
+                }
+            }
+        }
+    }
+}
